1ddf8397d05d31597591bb2f66a37ee844f90802
Gouspourd <gaspard.delpiano@gmail.com>
2022-05-01 14:24:14 +0200

    + and -

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ class Player(Sprite):
         self.dir = Vect2(0, 0)
         self.w = 64
         self.h = 64
+        self.pole = "-"
 
         self.is_magnetic = True
 
@@ -116,26 +117,31 @@ class Player(Sprite):
             self.state = "Grounded"
 
 class Magnetic_field:
-    def __init__(self,x = 0 ,y = 0 ,strength = 10 , rayon = 300):
+    def __init__(self,x = 0 ,y = 0 ,strength = 5 , pole = "+" , rayon = 100):
         self.pos = Vect2(x, y)
         self.strength = strength
         self.rayon = rayon
         self.is_magnetic = False
+        self.pole = pole
         
     def update(self):
         #TODO
         for actor in Globals.GAME.actors:
             if math.dist(self.pos.tuple(), actor.pos.tuple())<= self.rayon:
                 if actor.is_magnetic:
-                    actor.vel += (actor.pos-self.pos).normalized()*self.strength
+                    actor.vel += (actor.pos-self.pos).normalized()*self.strength*((self.pole != actor.pole)*2-1)
 
     def draw(self, surface: pygame.surface.Surface):
-        pygame.draw.circle(surface,Colors.BLUE,self.pos.tuple() ,self.rayon)
+        if self.pole == "+":
+            color = Colors.BLUE
+        else:
+            color = Colors.RED
+        pygame.draw.circle(surface,color,self.pos.tuple() ,self.rayon)
 
 class Game:
     def __init__(self):
         self.player = Player()
-        self.actors = [Magnetic_field(300,300),self.player]
+        self.actors = [Magnetic_field(300,300,5,"+"),Magnetic_field(600,300,5,"-"),self.player]
 
     # Game.update.getlook.fertilize.elevat.add.divide.getpos.x.world.sup.label.groud.is_maj()
 
